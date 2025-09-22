9a9da1948a5b5b35bd9919741dbb3bfa68dd5d3c
Bas-SDR <bas_reintsema@hotmail.com>
2025-09-22 16:44:37 +0200

    Added player model, need to add user model to integrate in player

--- src/main/java/org/basr/pinpoint/model/Player.java
+++ src/main/java/org/basr/pinpoint/model/Player.java
@@ -1,4 +1,23 @@
 package org.basr.pinpoint.model;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class Player {
+    private int playerId;
+    private User user;
+    private List<Integer> teamIds;
+    private List<Integer> leagueIds;
+    private double averageScore;
+    private Stats stats;
+
+    @Data
+    public static class Stats {
+        private int highestGame;
+        private int highestSeries;
+        private int totalPinfall;
+        private int averagePinfall;
+        private int perfectGames;
+    }
 }
