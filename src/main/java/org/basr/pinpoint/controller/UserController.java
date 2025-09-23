ad28a1abd28c1f873adde6d089def41df2598450
Bas-SDR <bas_reintsema@hotmail.com>
2025-09-23 15:28:46 +0200

    Added controller for user

--- src/main/java/org/basr/pinpoint/controller/UserController.java
+++ src/main/java/org/basr/pinpoint/controller/UserController.java
@@ -3,10 +3,7 @@ package org.basr.pinpoint.controller;
 import org.basr.pinpoint.model.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,5 +20,31 @@ public class UserController {
         this.userMap.put(user.getId(), user);
 
         return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<Long, User>> getAllUsers() {
+        return ResponseEntity.ok(this.userMap);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        if (this.userMap.containsKey(id)) {
+            return ResponseEntity.ok(this.userMap.get(id));
+        }
+        else {
+            return ResponseEntity.notFound().build();
+            }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        if (this.userMap.containsKey(id)) {
+            this.userMap.put(id, user);
+            return ResponseEntity.ok(user);
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
